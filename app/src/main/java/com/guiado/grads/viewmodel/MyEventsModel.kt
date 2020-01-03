def360f22cf4dc0a2290d37b754ebe6699fd5ca6
faizal <faizal.shah@philips.com>
2020-01-03 11:45:52 +0530

    order by causes issue

--- app/src/main/java/com/guiado/grads/viewmodel/MyEventsModel.kt
+++ app/src/main/java/com/guiado/grads/viewmodel/MyEventsModel.kt
@@ -1,6 +1,5 @@
 package com.guiado.grads.viewmodel
 
-
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -49,7 +48,7 @@ class MyEventsModel(internal var activity: FragmentActivity,
         } catch (e:Exception){
             Log.d(TAG, "getProfile  "+e)
         }
-        query = db.collection("events").orderBy("postedDate", Query.Direction.DESCENDING).limit(5).whereEqualTo("postedBy",mAuth.currentUser!!.uid)
+        query = db.collection("events").limit(5).whereEqualTo("postedBy",mAuth.currentUser!!.uid)
         doGetTalents()
     }
 
@@ -80,11 +79,10 @@ class MyEventsModel(internal var activity: FragmentActivity,
 
     }
 
+
     private fun handleMultipleClicks(): Boolean {
         return MultipleClickHandler.handleMultipleClicks()
     }
-
-
 
     private fun getCommbinationWords(s: String): List<String> {
         val list1 = s.sentenceToWords()
@@ -95,7 +93,7 @@ class MyEventsModel(internal var activity: FragmentActivity,
     fun doGetTalentsSearch(searchQuery: String) {
         query = db.collection("events")
                 .whereArrayContainsAny("searchTags", getCommbinationWords(searchQuery).toList())
-                .orderBy("postedDate", Query.Direction.DESCENDING).whereEqualTo("postedBy",mAuth.currentUser!!.uid)
+                .whereEqualTo("postedBy",mAuth.currentUser!!.uid)
                 .limit(5)
 
         Log.d(TAG, "DOIT doGetTalentsSearch: ")
@@ -156,7 +154,7 @@ class MyEventsModel(internal var activity: FragmentActivity,
             }
 
             val lastVisible = querySnapshot.documents[querySnapshot.size() - 1]
-            query = db.collection("events").orderBy("postedDate", Query.Direction.DESCENDING).limit(10).startAfter(lastVisible).whereEqualTo("postedBy",mAuth.currentUser!!.uid)
+            query = db.collection("events").limit(10).startAfter(lastVisible).whereEqualTo("postedBy",mAuth.currentUser!!.uid)
 
             for (change in querySnapshot.documentChanges) {
                 if (change.type == DocumentChange.Type.ADDED) {
--- app/src/main/java/com/guiado/grads/viewmodel/SavedEventsModel.kt
+++ app/src/main/java/com/guiado/grads/viewmodel/SavedEventsModel.kt
@@ -2,7 +2,10 @@ package com.guiado.grads.viewmodel
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupWindow
 import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 import androidx.databinding.ObservableArrayList
@@ -49,7 +52,6 @@ class SavedEventsModel(internal var activity: FragmentActivity,
             Log.d(TAG, "getProfile  "+e)
         }
         query = db.collection("events")
-                .orderBy("postedDate", Query.Direction.DESCENDING)
                 .limit(5).whereArrayContains("bookmarkBy",mAuth.currentUser!!.uid)
 
         doGetTalents()
@@ -80,7 +82,19 @@ class SavedEventsModel(internal var activity: FragmentActivity,
             bundle.putString(Constants.POSTAD_OBJECT, GenericValues().eventToString(postAdModel))
             fragment.setArguments(bundle)
             fragmentProfileInfo.mFragmentNavigation.pushFragment(fragmentProfileInfo.newInstance(1, fragment, bundle));
+        } else {
+            showPopUpWindow();
+        }
+    }
 
+    fun showPopUpWindow(){
+        val view = getNotificationContentView(activity,
+                activity.applicationContext.resources.getString(R.string.oops_title),
+                activity.applicationContext.resources.getString(R.string.oops_msg))
+        val popupWindow = PopupWindow(view, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        popupWindow.showAtLocation(view, Gravity.TOP, 0, 0);
+        view.findViewById<View>(R.id.closeBtn).setOnClickListener{
+            popupWindow.dismiss()
         }
     }
 
@@ -97,7 +111,6 @@ class SavedEventsModel(internal var activity: FragmentActivity,
     fun doGetTalentsSearch(searchQuery: String) {
         query = db.collection("events")
                 .whereArrayContainsAny("searchTags", getCommbinationWords(searchQuery).toList())
-                .orderBy("postedDate", Query.Direction.DESCENDING)
                 .limit(5).whereArrayContains("bookmarkBy",mAuth.currentUser!!.uid)
 
 
@@ -157,7 +170,6 @@ class SavedEventsModel(internal var activity: FragmentActivity,
             listItems.add(mAuth.currentUser!!.uid)
             val lastVisible = querySnapshot.documents[querySnapshot.size() - 1]
             query = db.collection("events")
-                    .orderBy("postedDate", Query.Direction.DESCENDING)
                     .limit(10).startAfter(lastVisible)
                     .whereArrayContains("bookmarkBy",mAuth.currentUser!!.uid)
 
