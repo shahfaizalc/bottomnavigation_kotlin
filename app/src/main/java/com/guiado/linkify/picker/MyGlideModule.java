77ccdf4a2a5a28ff15880a718fe7a393f024f3e6
faizal <faizal.shah@philips.com>
2020-02-04 12:34:14 +0530

    auditions

---
+++ app/src/main/java/com/guiado/linkify/picker/MyGlideModule.java
@@ -0,0 +1,8 @@
+package com.guiado.linkify.picker;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class MyGlideModule extends AppGlideModule {
+}
