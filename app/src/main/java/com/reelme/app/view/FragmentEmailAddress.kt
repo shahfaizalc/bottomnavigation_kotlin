1c529c286c52793dc6125869ca62434a01979dfb
shah faizal chanbasha <shahfaizalc@gmail.com>
2021-03-26 02:44:58 +0530

    no message

--- app/src/main/java/com/reelme/app/view/FragmentEmailAddress.kt
+++ app/src/main/java/com/reelme/app/view/FragmentEmailAddress.kt
@@ -1,7 +1,12 @@
 package com.reelme.app.view
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import androidx.databinding.DataBindingUtil
 import com.reelme.app.R
 import com.reelme.app.databinding.*
@@ -11,7 +16,7 @@ import com.reelme.app.viewmodel.*
 class FragmentEmailAddress : Activity() {
 
     @Transient
-    lateinit internal var areaViewModel: EmailAddressViewModel
+    internal lateinit var areaViewModel: EmailAddressViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,22 @@ class FragmentEmailAddress : Activity() {
         val binding : FragmentEmailaddressBinding = DataBindingUtil.setContentView(this, R.layout.fragment_emailaddress)
         areaViewModel = EmailAddressViewModel(this, this)
         binding.homeData = areaViewModel
+
+//        val spannable = SpannableStringBuilder(resources.getString(R.string.home_emailskip))
+//        spannable.setSpan(
+//                ForegroundColorSpan(Color.RED),
+//                8, // start
+//                12, // end
+//                Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+//        )
+
+        val spannable = SpannableString(resources.getString(R.string.home_emailskip))
+        spannable.setSpan(
+                ForegroundColorSpan(Color.WHITE),
+                37, resources.getString(R.string.home_emailskip).length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        binding.terms.text = spannable
     }
 
     override fun onResume() {
