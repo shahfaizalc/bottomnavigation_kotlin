80e567e9ab27d1bc127b185a042d4a63a4512717
shah faizal chanbasha <shahfaizalc@gmail.com>
2021-03-29 23:17:52 +0530

    no message

---
+++ app/src/main/java/com/reelme/app/view/FragmentDeleteAccount.kt
@@ -0,0 +1,37 @@
+package com.reelme.app.view
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.reelme.app.R
+import com.reelme.app.databinding.FragmentDeleteaccountBinding
+import com.reelme.app.databinding.FragmentDemographicsBinding
+import com.reelme.app.databinding.FragmentFullnameBinding
+import com.reelme.app.viewmodel.DeleteAccountViewModel
+import com.reelme.app.viewmodel.DemographicsViewModel
+import com.reelme.app.viewmodel.EnterFullNameViewModel
+
+
+class FragmentDeleteAccount : Activity() {
+
+    @Transient
+    internal lateinit var areaViewModel: DeleteAccountViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding : FragmentDeleteaccountBinding = DataBindingUtil.setContentView(this, R.layout.fragment_deleteaccount)
+        areaViewModel = DeleteAccountViewModel(this, this)
+        binding.homeData = areaViewModel
+    }
+
+    override fun onResume() {
+        super.onResume()
+        areaViewModel.registerListeners()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        areaViewModel.unRegisterListeners()
+    }
+}
