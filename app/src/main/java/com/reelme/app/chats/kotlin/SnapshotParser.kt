878812aee53c71cf954e8f9f3aa9728066d6ac56
shahfaizalc <shahfaizalc@gmail.com>
2021-06-19 09:50:27 +0530

    removed unused resources

--- app/src/main/java/com/reelme/app/chats/kotlin/SnapshotParser.kt
+++
@@ -1,8 +0,0 @@
-package com.reelme.app.chats.kotlin
-
-import com.google.firebase.firestore.DocumentSnapshot
-
-/**
- * Base interface for a [BaseSnapshotParser] for [DocumentSnapshot].
- */
-interface SnapshotParser<T> : BaseSnapshotParser<DocumentSnapshot?, T>
